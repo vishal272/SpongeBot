f5b10b34c7503f76a6d1c2a25393457dc8093f69
vishal272 <vishal27296@gmail.com>
2020-04-12 17:33:47 +0100

    Change channel name  for welcome to emote one

--- src/bot.js
+++ src/bot.js
@@ -16,7 +16,7 @@ const isValidCommand = (message, cmdName) => message.content.toLowerCase().start
 //Enforce Nickname On Welcome
 client.on('message', function(message) {
     if(message.author.bot) return;
-    if(message.channel.name === WelcomeChannel){
+    if(message.channel.name.slice(3) === "welcomee"){
         if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
         message.member.setNickname( message.author.username + " | " + message.content , " ").catch(err=>console.log(err));
         let { cache } = message.guild.roles;
@@ -29,10 +29,10 @@ client.on('message', function(message) {
                 message.channel.send("IGN added!")
                 setTimeout(() => {
                     message.channel.bulkDelete(100);
-                     message.member.roles.add(role);
+                    message.member.roles.add(role);
                 }, 2000);
                 setTimeout(() => {
-                    message.channel.send("Please enter your IGN to have full access of the server!<33")
+                    message.channel.send("Please enter your IGN to have full access of the server!:heart:")
                 }, 4000);
             }
        }
